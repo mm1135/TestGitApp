c19747d3fc4464824cf43b41297476fb713baf56
github mm1135 <sstana1132@gmail.com>
2022-06-19 12:33:08 +0900

    TestGitApp

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestGitApp
+//
+//  Created by 田中駿也 on 2022/06/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("TestGitApp")
+    }
+
+
+}
+
